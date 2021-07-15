8fd4419a7605b549b6882bb642eb7e75bac31a3b
eswaranthangavel23 <eswaran.thangavel@changecx.com>
2021-07-15 16:43:14 +0530

    Initial commit

---
+++ category.js
@@ -0,0 +1,156 @@
+import CategoryExporter from "@commercetools/category-exporter"
+import fs from 'fs'
+
+const options = {
+    apiConfig: {
+        apiUrl: 'https://api.us-central1.gcp.commercetools.com',
+        host: 'https://auth.us-central1.gcp.commercetools.com',
+        authUrl: 'https://auth.us-central1.gcp.commercetools.com',
+        projectKey: 'ccx_sunrise',
+        credentials: {
+            clientId: 'vSpnS-bOTfq4Bcz3EHO5KczW',
+            clientSecret: 'fF4kHHQSVSmuoPBd-ZlNsVPeZrZzVMYu',
+        }
+    },
+
+}
+
+const logger = {
+    error: console.error,
+    warn: console.warn,
+    info: console.log,
+    debug: console.debug,
+}
+
+const categoryExporter = new CategoryExporter.default(options, logger)
+var outputStream = fs.createWriteStream('categories.txt');
+// Register error listener
+outputStream.on('error', function(v) {
+    console.log(v);
+})
+
+outputStream.on('finish', () => {
+    fs.readFile('categories.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        var res = JSON.parse(data);
+
+        var parent = res;
+        var finalcategories = [];
+        for (let obj of parent) {
+            var category = {};
+            category.id = obj.id;
+            category.version = obj.version;
+            category.name = obj.name;
+            //category.slug = obj.slug;
+            //category.ancestors = obj.ancestors;
+            let lang = ['en', 'de', 'it'];
+            let i = 0;
+            if (obj.ancestors.length != 0) {
+                category.path = { en: "", de: "", it: "" }
+                category.slug = { en: "", de: "", it: "" }
+            }
+
+            for (let cat_obj of obj.ancestors) {
+                var cat = res.filter(x => x.id == cat_obj.id)[0];
+                for (let lan of lang) {
+
+                    if (category.path[lan] != "") {
+                        category.path[lan] += ' > ';
+                        category.slug[lan] += '/'
+                    }
+                    category.path[lan] += cat.name[lan]
+                    category.slug[lan] += cat.slug[lan]
+
+                }
+            }
+
+            if (category.path) {
+                category.path.en += ' > ' + obj.name.en;
+                if (category.path['de'] != "")
+                    category.path.de += ' > ';
+                category.path.de += obj.name.de;
+                category.path.it += ' > ' + obj.name.it;
+            } else {
+                category.path = category.name;
+                // console.log(category);
+            }
+
+            if (category.slug) {
+                category.slug.en += '/' + obj.slug.en;
+                if (category.slug['de'] != "")
+                    category.slug.de += '/';
+                category.slug.de += obj.slug.de;
+                category.slug.it += '/' + obj.slug.it;
+            } else {
+
+                category.slug = obj.slug;
+            }
+            finalcategories.push(category)
+
+        }
+        var categories = [];
+        for (let category of parent) {
+            var parent_object = {};
+            var object = [];
+            let i = 0;
+            let rootstr = "";
+            var jsonVariable = {};
+            parent_object.id = category.id;
+            parent_object.name = category.name.en;
+            parent_object.slug = category.slug.en
+            console.log(category, 'category')
+            let slug_url_str = "";
+            let cat_url_str = "";
+            for (let cat_slug_level of category.ancestors) {
+                var cat_slug = res.filter(obj => obj.id == cat_slug_level.id)[0];
+                //console.log(cat_slug, 'cat_slug')
+                slug_url_str += cat_slug.slug.en;
+                cat_url_str += cat_slug.name.en;
+                slug_url_str += '/';
+                cat_url_str += ' > '
+            }
+            slug_url_str += category.slug.en;
+            cat_url_str += category.name.en
+                //console.log(slug_url_str)
+            for (let cat_level of category.ancestors) {
+                var cat_parent = res.filter(obj => obj.id == cat_level.id);
+                let str = "lvl" + i;
+                rootstr += cat_parent[0].name.en;
+                jsonVariable[str] = rootstr;
+                jsonVariable['lvl' + i] = rootstr;
+                rootstr += ' > '
+                i++;
+            }
+            jsonVariable['lvl' + i] = rootstr + category.name.en;
+            console.log(rootstr + category.name.en)
+            object.push(jsonVariable);
+            parent_object.categories = object;
+            parent_object.categories_level = i;
+            parent_object.slugURL = slug_url_str;
+            parent_object.categoryURL = cat_url_str;
+            parent_object.parent = category.parent;
+            categories.push(parent_object);
+
+        }
+        fs.writeFile('categories.json', JSON.stringify(categories), 'utf8', function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+        fs.writeFile('finalcategories.json', JSON.stringify(finalcategories), 'utf8', function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+        console.log('done with export')
+    })
+})
+
+
+
+categoryExporter.run(outputStream)
---
+++ product.js
@@ -0,0 +1,96 @@
+import ProductExporter from '@commercetools/product-exporter'
+import fs from 'fs'
+import algoliasearch from 'algoliasearch'
+const client = algoliasearch('RA46FT2Q5G', '69a1fff0143c7121bc2f35825680b272')
+const index = client.initIndex('CTL_Syn_test')
+
+const apiConfig = {
+    apiUrl: 'https://api.us-central1.gcp.commercetools.com',
+    host: 'https://auth.us-central1.gcp.commercetools.com',
+    authUrl: 'https://auth.us-central1.gcp.commercetools.com',
+    projectKey: 'ccx_sunrise',
+    credentials: {
+        clientId: 'vSpnS-bOTfq4Bcz3EHO5KczW',
+        clientSecret: 'fF4kHHQSVSmuoPBd-ZlNsVPeZrZzVMYu',
+    }
+}
+const exportConfig = {
+    batch: 10,
+    json: true,
+    staged: true,
+    total: 100,
+}
+const logger = {
+    error: console.error,
+    warn: console.warn,
+    info: console.log,
+    debug: console.debug,
+}
+const accessToken = 'PkAXtg8S4W0MLo0nt6m8sHPG21piVc5g'
+
+const productExporter = new ProductExporter.default(
+    apiConfig,
+    exportConfig,
+    logger,
+    accessToken,
+)
+var outputStream = fs.createWriteStream('products.txt');
+// Register error listener
+outputStream.on('error', function(v) {
+    console.log(v);
+})
+outputStream.on('finish', () => {
+    fs.readFile('categories.json', (err, data) => {
+        if (err) throw err;
+
+        var categories = JSON.parse(data);
+        fs.readFile('products_raw.json', (err, data) => {
+            if (err) throw err;
+            let products = JSON.parse(data);
+            var finalproducts = []
+            for (let product of products) {
+                let k = product.categories.length;
+                if (k != 0) {
+                    console.log(product, product.categories[(k != 0 ? k - 1 : k)]);
+                    let parent_cat = categories.filter(x => x.id == product.categories[(k != 0 ? k - 1 : k)].id)[0];
+                    product.categories.push(parent_cat.parent);
+                }
+            }
+            for (let product of products) {
+                let j = 0;
+                let k = product.categories.length;
+                for (let cat of product.categories) {
+                    if (cat) {
+                        var obj = categories.filter(x => x.id == cat.id)[0];
+                        product.categories[j].slug = obj.slugURL;
+                        product.categories[j]['lvl' + (k != 0 ? k - 1 : k)] = obj.categoryURL;
+                        j++;
+                        k--;
+                    }
+                }
+                if (product.categories[0]) {
+                    var h_categories = categories.filter(x => product.categories[0].id == x.id)[0].categories;
+                    product.hierarchicalCategories = h_categories;
+                } else
+                    product.hierarchicalCategories = [];
+                finalproducts.push(product);
+            }
+            // index
+            //     .saveObjects(finalproducts, { autoGenerateObjectIDIfNotExist: true })
+            //     .then(() => {
+
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     });
+            fs.writeFile('finalproducts.json', JSON.stringify(finalproducts), 'utf8', function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+            });
+        });
+    })
+})
+
+productExporter.run(outputStream)
